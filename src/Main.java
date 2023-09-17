8330b7ed5b82980f27f7b99ba2c4b293f580fa44
captain-dj <whardig10@gmail.com>
2023-09-17 19:44:45 -0400

    V1.2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Your price is 50$ and a a 5% increese");
+        System.out.println("Hello Your credit card balance is 5000$ with intrest of 17%");
 
-        double doubleOperandA = 100;
-        double doubleOperandB = 0.05;
-        double doubleOperandC = 50;
-        double doubleSum = 0;
-        double TaxSum = 0;
+        double doubleOperandBase = 5000;
+        double doubleOperandIntrest = 0.17;
+        double doubleOperandTime = 30;
+        double Month1Sum = 0;
+        double Month2Sum = 0;
 
-        doubleSum = doubleOperandB / doubleOperandA;
-        TaxSum = doubleOperandC * doubleSum;
-        System.out.println("After the Tax your item is "+ TaxSum +" ");
+// I = principal * rate * time
+        Month1Sum = doubleOperandBase * doubleOperandIntrest * doubleOperandTime;
+        System.out.println("Your intrest after 1 month is "+ Month1Sum +" ");
+
+        Month2Sum = Month1Sum * doubleOperandIntrest * doubleOperandTime;
+        System.out.println("Your intrest after 2 months is "+ Month2Sum +" ");
 
 
     }
